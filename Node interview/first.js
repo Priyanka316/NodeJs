f3faf98008b31184af451da7cf6cb8125c40dbac
Priyanka316 <107699187+Priyanka316@users.noreply.github.com>
2022-11-23 17:44:42 +0530

    Add files via upload

---
+++ Node interview/first.js
@@ -0,0 +1,25 @@
+var addFn = require('./index.js');
+//1
+// var operatorsObj = require('./index.js');
+
+//4
+// var add = require('./index.js');
+
+//5
+// var {add} = require('./index.js');
+
+function greet(name){
+    console.log("hello there,"+name+"!");
+}
+
+greet("Priyanka");
+addFn(10, 20);
+
+//1
+// operatorsObj.add(10,20);
+
+//4
+// add(10,20);
+
+//5
+// console.log(add(10,20))
